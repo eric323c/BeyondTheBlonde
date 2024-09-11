880987b358d627ea7e1de4a76a5c43e8a99c7ad4
eric323c <71191021+eric323c@users.noreply.github.com>
2024-09-11 14:01:07 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
 
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h1>Beyond the Blonde</h1>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/services">Services</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <button className="book-now">Book Now</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
